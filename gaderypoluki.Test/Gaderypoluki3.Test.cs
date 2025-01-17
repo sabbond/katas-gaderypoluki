be2dd934d504b80802810887cd0df3c97bcd36ca
sabbond <sab@sabisme.uk>
2025-01-17 12:11:44 +0000

    Added more test data, tweaks to tests

--- gaderypoluki.Test/Gaderypoluki3.Test.cs
+++ gaderypoluki.Test/Gaderypoluki3.Test.cs
@@ -2,7 +2,7 @@ namespace gaderypoluki.Test;
 using FluentAssertions;
 
 [TestClass]
-public class gaderypoluki3Test
+public class Gaderypoluki3Test
 {
     public static IEnumerable<Object[]> TestData
     {
@@ -14,6 +14,16 @@ public class gaderypoluki3Test
                     new string[] {"dance on the table", "hide my beers", "scouts rocks"},
                     new string[] {"egncd pn thd tgbud" ,"hked mr bddys" , "scplts ypcis"},
                     "agdeikluopry"
+                ],
+                [
+                    new string[] {"dance on the table", "hide my beers", "scouts rocks"},
+                    new string[] {"dkucr pu yhr ykbir", "hldr mt brres", "scpnys epcas"},
+                    "akerilnuopty"
+                ],
+                [
+                    new string[] {"THE QUICK BROWN FOX jumps over the LAZY BOY"},
+                    new string[] {"UHC QTNEO BYKWI FKX jtaps kvcy uhc LMZR BKR"},
+                    "amceinkorytu"
                 ]
             ];
         }
@@ -21,19 +31,22 @@ public class gaderypoluki3Test
 
     [TestMethod]
     [DynamicData(nameof(TestData))]
-    public void Gaderypoluki3_GeneratesCorrectKeyForInput(string[] messages, string[] secrets, string expected){
+    public void Gaderypoluki3_GeneratesCorrectKeyForInput(string[] messages, string[] secrets, string expected)
+    {
         Gaderypoluki3.FindKey(messages, secrets).Should().BeEquivalentTo(expected);
     }
 
     [TestMethod]
-    public void Gaderypoluki3_throwsWhenInputsLengthDoesntMatch(){
-        Action action = () => Gaderypoluki3.FindKey(new[] {"123456"}, new[] {"123"});
+    public void Gaderypoluki3_throwsWhenInputsLengthDoesntMatch()
+    {
+        Action action = () => Gaderypoluki3.FindKey(["123456"], ["123"]);
         action.Should().Throw<ArgumentException>();
     }
 
     [TestMethod]
-    public void Gaderypoluki3_throwsWhenInputEncodingWrong(){
-        Action action = () => Gaderypoluki3.FindKey(new[] {"hide my beers"}, new[] {"hked mr bdlys"});
+    public void Gaderypoluki3_throwsWhenInputEncodingInconsistent()
+    {
+        Action action = () => Gaderypoluki3.FindKey(["hide my beers"], ["hked mr bdlys"]);
         action.Should().Throw<ArgumentException>();
     }
 }
