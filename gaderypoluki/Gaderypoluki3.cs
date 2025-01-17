8ada048a930e00de89dca0ac2f46fce3e93b7897
sabbond <sab@sabisme.uk>
2025-01-17 14:13:46 +0000

    edited for readability

--- gaderypoluki/Gaderypoluki3.cs
+++ gaderypoluki/Gaderypoluki3.cs
@@ -4,22 +4,22 @@ public static class Gaderypoluki3
 {
     public static string FindKey(string[] messages, string[] secrets)
     {
+        // use a hashset so each key is only stored once
         var keys = new HashSet<String>();
-        var bigMessage = string.Concat(messages).ToLowerInvariant().ToCharArray();
-        var bigSecret = string.Concat(secrets).ToLowerInvariant().ToCharArray();
-        if (bigMessage.Length != bigSecret.Length)
+        var allMessages = string.Concat(messages).ToLowerInvariant().ToCharArray();
+        var allSecrets = string.Concat(secrets).ToLowerInvariant().ToCharArray();
+        if (allMessages.Length != allSecrets.Length)
         {
             throw new ArgumentException("Messages and Secrets should be the same length");
         }
-        for (int count = 0; count < bigMessage.Length; count++)
+        for (int count = 0; count < allMessages.Length; count++)
         {
-            var messageChar = bigMessage[count];
-            var secretChar = bigSecret[count];
+            var messageChar = allMessages[count];
+            var secretChar = allSecrets[count];
             if (messageChar != secretChar)
             {
-                // char has been encoded, add before and after to the keys
-                // but order them alphabetically
-                // (sorting probs inefficient with 2 values, use if)
+                // char has been encoded, add both values to the keys
+                // order them alphabetically
                 if (messageChar < secretChar)
                 {
                     keys.Add(string.Concat(messageChar, secretChar));
@@ -30,10 +30,9 @@ public static class Gaderypoluki3
                 }
             }
         }
-        // invalid input could give us more than one mapping per character
-        // validate keys before return
+        // concatenate keys to give us the key
         var key = String.Concat(keys.OrderBy(key => key));
-        // if any letter appears more than once then the key is invalid
+        // invalid input could give us more than one mapping per character
         if (key.GroupBy(c => c).Any(g => g.Count() > 1)) {
             throw new ArgumentException("Inconsistent mappings in data, key invalid");
         }
